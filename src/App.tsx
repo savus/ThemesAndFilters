db14d640f64379507fda44c9e0c1f9d90e0791f5
Savus <justin.fuzzygreenstuff@gmail.com>
2023-11-24 06:30:56 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import "./css/base.css";
+import "./css/theme.css";
+import "./css/styles.css";
+
+function App() {
+
+  return (
+    <>
+      <main className="main-section">
+        <header className="main-header">
+          <nav className="navbar">
+            <ul className="navbar-nav ul-defaults-none">
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+              <li>Item 4</li>
+            </ul>
+          </nav>
+        </header>
+      </main>
+    </>
+  )
+}
+
+export default App
