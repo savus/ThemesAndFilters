07e4b04564ac9afd08c746670a85afaec0e4d9bd
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2023-12-10 11:41:17 -0700

    need to check why switcher-btns click event listener isn't working

--- src/js/main.js
+++ src/js/main.js
@@ -15,14 +15,21 @@ const toggleTheme = document.querySelector(themeTab);
 const switcher = document.querySelectorAll(switcherBtn);
 const currentTheme = localStorage.getItem(theme);
 
-toggleTheme.addEventListener('click', function() {
-    const tab = document.getElementById(tabId);
-    if (!tab.className.includes(open)) tab.classList.add(open);
-    else tab.classList.remove(open);
+toggleTheme.addEventListener("click", function () {
+  const tab = document.getElementById(tabId);
+  if (!tab.className.includes(open)) tab.classList.add(open);
+  else tab.classList.remove(open);
 });
 
+const setActive = (element, selector) => {
+  if (document.querySelector(`${selector}.${active}`) !== null)
+    document.querySelector(`${selector}.${active}`).classList.remove(active);
+  element.classList.add(active);
+};
+
 for (const elm of switcher) {
-    elm.addEventListener('click', function() {
-        
-    })
+  elm.addEventListener("click", function () {
+    setActive(elm, switcherBtn);
+    console.log("clicked");
+  });
 }
